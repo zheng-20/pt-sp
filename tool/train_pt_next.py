d53c4e3e754fbd8114c9acb3c6366d5a23b40a0a
Zheng Fang <fangzheng20@mails.ucas.ac.cn>
2023-10-16 23:49:27 +0800

    添加纯语义评价指标

--- tool/train_pt_next.py
+++ tool/train_pt_next.py
@@ -527,9 +527,9 @@ def validate(val_loader, model, criterion, boundary_criterion):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     # loss_meter = AverageMeter()
-    # intersection_meter = AverageMeter()
-    # union_meter = AverageMeter()
-    # target_meter = AverageMeter()
+    intersection_meter = AverageMeter()
+    union_meter = AverageMeter()
+    target_meter = AverageMeter()
     feat_loss_meter = AverageMeter()
     type_loss_meter = AverageMeter()
     boundary_loss_meter = AverageMeter()
@@ -577,13 +577,13 @@ def validate(val_loader, model, criterion, boundary_criterion):
         #     n = count.item()
         #     loss /= n
 
-        # intersection, union, target = intersectionAndUnionGPU(output, target, args.classes, args.ignore_label)
-        # if args.multiprocessing_distributed:
-        #     dist.all_reduce(intersection), dist.all_reduce(union), dist.all_reduce(target)
-        # intersection, union, target = intersection.cpu().numpy(), union.cpu().numpy(), target.cpu().numpy()
-        # intersection_meter.update(intersection), union_meter.update(union), target_meter.update(target)
+        intersection, union, target = intersectionAndUnionGPU(type_per_point.max(1)[1], semantic, args.classes, args.ignore_label)
+        if args.multiprocessing_distributed:
+            dist.all_reduce(intersection), dist.all_reduce(union), dist.all_reduce(target)
+        intersection, union, target = intersection.cpu().numpy(), union.cpu().numpy(), target.cpu().numpy()
+        intersection_meter.update(intersection), union_meter.update(union), target_meter.update(target)
 
-        # accuracy = sum(intersection_meter.val) / (sum(target_meter.val) + 1e-10)
+        accuracy = sum(intersection_meter.val) / (sum(target_meter.val) + 1e-10)
         # loss_meter.update(loss.item(), n)
         
         # softmax = torch.nn.Softmax(dim=1)
@@ -690,6 +690,7 @@ def validate(val_loader, model, criterion, boundary_criterion):
         # spec_cluster_pred = block_mean_shift_gpu(coord, primitive_embedding, offset, bandwidth=args.bandwidth)
         spec_cluster_pred = mean_shift_gpu(primitive_embedding, offset, bandwidth=args.bandwidth)
         s_iou, p_iou = compute_iou(label, spec_cluster_pred, type_per_point, semantic, offset)
+        # s_iou, p_iou = 0, 0
         # All Reduce loss
         if args.multiprocessing_distributed:
             # dist.all_reduce(feat_loss.div_(torch.cuda.device_count()))
@@ -724,16 +725,16 @@ def validate(val_loader, model, criterion, boundary_criterion):
                                                           s_iou_meter=s_iou_meter,
                                                           type_iou_meter=type_iou_meter))
 
-    # iou_class = intersection_meter.sum / (union_meter.sum + 1e-10)
-    # accuracy_class = intersection_meter.sum / (target_meter.sum + 1e-10)
-    # mIoU = np.mean(iou_class)
-    # mAcc = np.mean(accuracy_class)
-    # allAcc = sum(intersection_meter.sum) / (sum(target_meter.sum) + 1e-10)
+    iou_class = intersection_meter.sum / (union_meter.sum + 1e-10)
+    accuracy_class = intersection_meter.sum / (target_meter.sum + 1e-10)
+    mIoU = np.mean(iou_class)
+    mAcc = np.mean(accuracy_class)
+    allAcc = sum(intersection_meter.sum) / (sum(target_meter.sum) + 1e-10)
 
     if main_process():
-        # logger.info('Val result: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(mIoU, mAcc, allAcc))
-        # for i in range(args.classes):
-        #     logger.info('Class_{} Result: iou/accuracy {:.4f}/{:.4f}.'.format(i, iou_class[i], accuracy_class[i]))
+        logger.info('Val result: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(mIoU, mAcc, allAcc))
+        for i in range(args.classes):
+            logger.info('Class_{} Result: iou/accuracy {:.4f}/{:.4f}.'.format(i, iou_class[i], accuracy_class[i]))
         logger.info('Val result: Seg_mIoU/Type_mIoU {:.4f}/{:.4f}.'.format(s_iou_meter.avg, type_iou_meter.avg))
         logger.info('<<<<<<<<<<<<<<<<< End Evaluation <<<<<<<<<<<<<<<<<')
 
